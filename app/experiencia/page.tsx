acb575b4c077e8cff272e3dbaff932b21a1b4211
Marlon Jerold <marlonjerold@outlook.com.br>
2025-01-20 04:01:53 -0300

    agora foi

--- app/experiencia/page.tsx
+++ app/experiencia/page.tsx
@@ -1,8 +1,5 @@
 import Header from '@/components/Header';
-import About from '@/components/About'
-import Projects from '@/components/Projects'
 import Experience from '@/components/Experience'
-import Streaming from '@/components/Streaming'
 
 export default function Home() {
   return (
