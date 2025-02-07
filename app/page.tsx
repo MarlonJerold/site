5db27c5992bf63347e9eafc637fa36ff25f5ebbd
Marlon Jerold <marlonjerold@outlook.com.br>
2025-02-07 18:43:45 -0300

    bora

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,6 @@ export default function Home() {
       <Header />
       <About />
       <Streaming />
-      <section className="flex flex-col items-center justify-center min-h-[50vh]  space-y-6">
-  <p className="text-lg text-center text-[#c9c9c9] max-w-lg">
-    O que passa na mente de um pato que escreve códigos
-  </p>
-</section>
     </main>
   )
 }
